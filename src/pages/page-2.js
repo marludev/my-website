1f381c7509ac1e6ef5cc31df775ea9acb04f11df
HeySoyGabo <luisgmp1997@gmail.com>
2020-09-14 10:57:35 -0400

    first commit

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,20 @@
-import React from "react"
-import { Link } from "gatsby"
+import { Link } from 'gatsby'
+// eslint-disable-next-line no-use-before-define
+import React from 'react'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = () => {
+  const hola = 123
+  return (
+    <Layout>
+      <SEO title="Page two" />
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2</p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
 
 export default SecondPage
