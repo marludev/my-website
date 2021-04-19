2e03f1e9c18e061413538af721b6661d09845d67
Luis Gabriel Martínez <53130545+marludev@users.noreply.github.com>
2021-04-19 17:48:30 -0400

    feat: new componente seo

--- src/components/molecules/Seo/Seo.tsx
+++ src/components/molecules/Seo/Seo.tsx
@@ -1,77 +1,41 @@
 import React from 'react'
-import Head from 'next/head'
 import { useRouter } from 'next/dist/client/router'
 import { IProps } from './type'
+import { NextSeo, NextSeoProps } from 'next-seo'
 
-const SEO: React.FC<IProps> = ({
+const SEO = ({
   isTemplate = true,
   type = 'website',
-  date,
-  children,
   ...props
-}) => {
+}: IProps & NextSeoProps) => {
   const router = useRouter()
-
-  const title = props.title
+  // TODO recuerda acomodar esto
+  const title = props.title ? props.title : 'title default'
   const parsedTitle = isTemplate ? '%s | @marludev'.replace('%s', title) : title
+  const description = props.description
+  const shareImage = props.shareImage ? props.shareImage : 'imagen default'
 
   return (
-    <Head>
-      <title>{parsedTitle}</title>
-      <meta name="robots" content="follow, index" />
-      <meta key="description" content={props.description} name="description" />
-      <meta
-        key="og:url"
-        property="og:url"
-        content={`https://marludev.com${router.asPath}`}
-      />
-      <meta key="og:type" property="og:type" content={type} />
-      <meta
-        key="og:site_name"
-        property="og:site_name"
-        content="Daniel Esteves"
-      />
-      <meta
-        key="og:description"
-        property="og:description"
-        content={props.description}
-      />
-      <meta key="og:title" property="og:title" content={parsedTitle} />
-      <meta key="og:image" property="og:image" content={props.shareImage} />
-      <meta key="og:image:type" property="og:image:type" content="image/jpeg" />
-      <meta key="og:image:alt" property="og:image:alt" content={parsedTitle} />
-      <meta
-        key="twitter:card"
-        name="twitter:card"
-        content="summary_large_image"
-      />
-      <meta key="twitter:site" name="twitter:site" content="@danestves" />
-      <meta key="twitter:creator" name="twitter:creator" content="@danestves" />
-      <meta key="twitter:title" name="twitter:title" content={parsedTitle} />
-      <meta
-        key="twitter:description"
-        name="twitter:description"
-        content={props.description}
-      />
-      <meta
-        key="twitter:image"
-        name="twitter:image"
-        content={props.shareImage}
-      />
-      <meta
-        key="twitter:image:alt"
-        name="twitter:image:alt"
-        content={parsedTitle}
-      />
-      {date && (
-        <meta
-          key="article:published_time"
-          property="article:published_time"
-          content={date}
-        />
-      )}
-      {children}
-    </Head>
+    <NextSeo
+      title={parsedTitle}
+      description={description}
+      canonical={`https://marludev.com/${router.asPath}`}
+      openGraph={{
+        url: `https://marludev.com/${router.asPath}`,
+        title: parsedTitle,
+        description,
+        images: [{ url: shareImage }],
+        site_name: parsedTitle,
+        type,
+        locale: 'es',
+      }}
+      twitter={{
+        handle: '@marludev',
+        site: '@marludev',
+        cardType: 'summary_large_image',
+      }}
+      {...props}
+    />
   )
 }
 
--- src/components/molecules/Seo/type.ts
+++ src/components/molecules/Seo/type.ts
@@ -1,6 +1,6 @@
 import React from 'react'
 export interface IProps {
-  title: string
+  title?: string
   isTemplate?: boolean
   description?: string
   shareImage?: string
