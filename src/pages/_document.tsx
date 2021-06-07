f9e3581e3f9792bcfec956f0a288ad874c3c2999
luis <marludev@DESKTOP-JV85QCJ.localdomain>
2021-06-07 13:54:10 -0400

    :mag: [feat]: add charset utf-8

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="es">
-        <Head />
+        <Head>
+          <meta charSet="utf-8" />
+        </Head>
         <body>
           <Main />
           <NextScript />
