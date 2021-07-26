0ecde4163517a5e502e75603c9b53732278d3931
luis <luisgmp1997@gmail.com>
2021-07-26 15:04:29 -0400

    feat: code searh console

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,10 @@ class MyDocument extends Document {
     return (
       <Html lang="es">
         <Head>
+          <meta
+            name="google-site-verification"
+            content="_-Rlumz1aPyyWCi_S28LCy7_UX9oBAcgHWc1mdL7TE8"
+          />
           <meta charSet="utf-8" />
         </Head>
         <body>
