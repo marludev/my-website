98eaf6dbfe01062a3274e54c754c61bcbb34efb6
Luis Martinez <luisgmp1997@gmail.com>
2022-08-15 19:48:26 -0400

    fix problem with sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
 const config = {
   siteUrl: 'https://marludev.com',
-  generateRobotsTxt: true,
+  generateRobotsTxt: true, // (optional)
+  // ...other options
 }
 
 export default config
